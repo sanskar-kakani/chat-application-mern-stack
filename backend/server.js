51bee2e23cd8489cf8920a0fff82844edeb24a2c
SANSKAR KAKANI <sanskarkakani01@gmail.com>
2023-10-13 13:39:22 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const cors = require("cors");
 
 
 connectDB();
